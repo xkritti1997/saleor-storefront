b7f07a2e33dcb8c29b9d3141d71e1a345d010aaf
krittamet.p <krittamet.p@digital.organic>
2021-05-14 17:25:01 +0700

    add pricePage

--- src/views/Home/Page.tsx
+++ src/views/Home/Page.tsx
@@ -1,7 +1,7 @@
 import "./scss/index.scss";
 
 import classNames from "classnames";
-import * as React from "react";
+import React, { useState } from "react";
 import { FormattedMessage, useIntl } from "react-intl";
 import { Link } from "react-router-dom";
 
@@ -18,6 +18,8 @@ import { structuredData } from "../../core/SEO/Homepage/structuredData";
 
 import noPhotoImg from "../../images/no-photo.svg";
 
+import axios from "axios";
+
 const Page: React.FC<{
   loading: boolean;
   categories: ProductsList_categories;
@@ -28,6 +30,40 @@ const Page: React.FC<{
     return categories && categories.edges && categories.edges.length > 0;
   };
   const intl = useIntl();
+
+  const [seconds, setSeconds] = useState(1);
+  const [goldres, setGoldres] = useState([]);
+  const [gold_date, setgold_date] = useState("");
+
+  React.useEffect(() => {
+    async function call_api_gold() {
+      const currentDate = new Date();
+      const timestamp = currentDate.getTime();
+      const url_gold = `https://chinhuaheng.com/gold/now?=${timestamp}`;
+      const res = await axios.get(
+        `https://cors-anywhere.herokuapp.com/${url_gold}`
+      );
+      const golddate = new Date(Date.parse(res.data["date"]));
+      setGoldres(goldres => res.data);
+      console.log(res.data, timestamp);
+      setgold_date(gold_date => golddate.toString());
+      console.log(gold_date);
+      setSeconds(timestamp);
+    }
+
+    if (seconds === 1) {
+      call_api_gold();
+    } else {
+      const interval = setInterval(async () => {
+        try {
+          call_api_gold();
+        } catch (error) {
+          console.log(error, seconds);
+        }
+      }, 60000);
+      return () => clearInterval(interval);
+    }
+  }, [seconds]);
 
   return (
     <>
@@ -77,6 +113,141 @@ const Page: React.FC<{
           )}
         </div>
       </div>
+
+      <div
+        className="container"
+        style={{
+          fontFamily: "Prompt, sans-serif",
+          minHeight: "196px",
+          borderRadius: "10px",
+        }}
+      >
+        <div
+          style={{
+            backgroundColor: "#a4ebf3",
+            marginTop: "5px",
+            padding: "5px 0px 5px 0px",
+            fontSize: "25px",
+            color: "black",
+            textAlign: "center",
+          }}
+        >
+          <h5>ราคาทองวันนี้</h5>
+        </div>
+        <div
+          style={{
+            backgroundColor: "#ccf2f4",
+            height: "100%",
+            paddingBottom: "15px",
+          }}
+        >
+          {seconds > 1 ? (
+            <span
+              style={{
+                fontSize: 14,
+              }}
+            >
+              ราคาทองล่าสุด : {gold_date}
+              (ครั้งที่ {goldres["gold"]["announce"]})
+            </span>
+          ) : (
+            <span>loading ...</span>
+          )}
+          <div
+            style={{
+              border: "1px solid gold",
+              backgroundColor: "#f4f9f9",
+              margin: "10px 15px",
+              padding: "10px 15px",
+              verticalAlign: "middle",
+              borderRadius: "10px",
+              textAlign: "center",
+            }}
+          >
+            <span
+              style={{
+                fontSize: "18px",
+                fontWeight: "bold",
+              }}
+            >
+              ราคาทองคำแท่ง
+            </span>
+            <div
+              className="container"
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-around",
+                alignItems: "center",
+                flexWrap: "wrap",
+                marginTop: "15px",
+              }}
+            >
+              <div
+                style={{
+                  display: "flex",
+                  flex: 1,
+                  flexDirection: "column",
+                  justifyContent: "space-around",
+                  alignItems: "center",
+                }}
+              >
+                <span
+                  style={{
+                    fontSize: "18px",
+                    fontWeight: "bold",
+                    marginBottom: "15px",
+                  }}
+                >
+                  ขายออก
+                </span>
+                <span style={{ fontSize: "18px", fontWeight: "bold" }}>
+                  รับซื้อ
+                </span>
+              </div>
+
+              <div
+                style={{
+                  display: "flex",
+                  flex: 1,
+                  flexDirection: "column",
+                  justifyContent: "space-around",
+                  alignItems: "center",
+                }}
+              >
+                {seconds > 1 ? (
+                  <span
+                    style={{
+                      fontSize: "18px",
+                      fontWeight: "bold",
+                      marginBottom: "15px",
+                      color: "red",
+                    }}
+                  >
+                    {goldres["gold"]["gta_offer"]}
+                  </span>
+                ) : (
+                  <span>loading ...</span>
+                )}
+                {seconds > 1 ? (
+                  <span
+                    style={{
+                      color: "green",
+                      fontSize: "18px",
+                      fontWeight: "bold",
+                    }}
+                  >
+                    {goldres["gold"]["gta_bid"]}
+                  </span>
+                ) : (
+                  <span>loading ...</span>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
       <ProductsFeatured
         title={intl.formatMessage({ defaultMessage: "Featured" })}
       />
